a4040a1856dd960e372f1f1bac59bd6fba3504f7
Manel Rhaiem <mrhaiem@mrhaiem-10117.local>
2016-07-15 16:40:50 +0100

    length request should be less

--- logging.ino
+++ logging.ino
@@ -1,40 +1,46 @@
-char body[]="v=1&t=event&tid=UA-77033033-1&cid=9eccb690-93aa-4513-835a-9a4f0f0e2a71&ec=category&ea=action&el=label&ev=1&an=iot_app&av=1.0&ul=en-us&cd1=linux&cd2=1.2&cd3=RPi%2F2&cd4=arm&cd5=rust%20test&cd6=20160320123456&cd7=2016-05-25%2022:36:57";
+char body[]="v=1&t=event&tid=UA-77033033-1&cid=9eccb690-93aa-4513-835a-9a4f0f0e2a71&ec=ManelCategory&ea=action&el=label&ev=1&an=iot_app&av=1.0&ul=en-us&cd1=linux&cd2=1.2&cd3=RPi%2F2&cd4=arm&cd5=rust%20test&cd6=20160320123456&cd7=2016-05-25%2022:36:57";
 #include <string.h>
+#define DOMAIN "analytics.google.com"
 TCPClient client;
+bool connecte = false;
 void setup()
 {
+  delay(1000);
   Serial.begin(9600);
-  while(!Serial.available()) Particle.process();
+  while(!Serial.available()) SPARK_WLAN_Loop();
   Serial.println("connecting....");
-  if (client.connect("https://www.google-analytics.com/collect", 80))
-  {
-    Serial.println("connected");
-    client.println("POST /update HTTP/1.0");
-    client.println("Host: https://www.google-analytics.com/collect");
-    client.println("Content-Length: ");
-    client.println(strlen(body)+14);
-    client.println();
-    client.println(body);
-  }
-  else
-  {
-    Serial.println("connection failed");
-  }
+  int cnx = client.connect(DOMAIN, 80);
+    //Serial.println("connected");
+    if (cnx == 1) {
+            Serial.println("connexion ok.");
+            connecte = true;
+            client.println("POST /collect HTTP/1.1");
+            client.println("Host: analytics.google.com");
+            client.println("");
+            client.print("Content-Length: ");
+            client.println(strlen(body));
+            client.println("");
+            client.println(body);
+            Serial.println(strlen(body));
+        } else {
+            Serial.println("connexion echouee, erreur : ");
+            Serial.print(cnx, DEC);
+        }
+
+
 }
 
 void loop() {
-  if (client.available())
-    {
-      char c = client.read();
-      Serial.print(c);
+  if (connecte) {
+        if (client.available()) {
+            char c = client.read();
+            Serial.print(c);
+        }
+        if (!client.connected()) {
+            Serial.println();
+            Serial.println("deconnexion.");
+            connecte = false;
+            client.stop();
+        }
     }
-
-    if (!client.connected())
-    {
-      Serial.println();
-      Serial.println("disconnecting.");
-      client.stop();
-      for(;;);
-    }
-
 }
